5c6463f79ae4100c38f92598740cf7342ff7a7e3
Nihar1066 <niharnadia1020@gmail.com>
2026-07-10 17:43:22 +0530

    Add cookie-parser and cors middleware, implement error handling classes, and set up server initialization

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
-connectDB();
 dotenv.config({
     path: './env'
 })
+
+const port= process.env.PORT || 8000;
+connectDB()
+.then(()=>{
+    app.listen(port ,()=>{
+        console.log(`app is listening on http://localhost:${port}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo Db connection failed!!!!")
+})
+
 // import express from "express"
 
 // const app= express();
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,9 @@
+class apiResponse{
+    constructor(statusCode,data,message="Success"){
+        this.statusCode=statusCode;
+        this.data=null;
+        this.message=message;
+        this.success=statusCode <400;
+
+    }
+}
